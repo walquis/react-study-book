68eeaf1c5b367473b002d3573c4d7ca356f708ea
Chris Walquist <chris@walquist.net>
2022-10-22 08:30:48 -0500

    useState() wires up a variable to a location in a global state object, along with (optionally) a setter function for that location.
    useEffect() wires up code to change-events happening to a specific location in the state object--and makes that location available.

--- react-hooks-primer/src/App.js
+++ react-hooks-primer/src/App.js
@@ -1,9 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [weight, setWeight] = useState(150);
+  const [age, setAge] = useState(42);
+  const [month] = useState('February');
+  const [todos] = useState([{ text: 'Eat pie' },{text: 'Blow out candles'}]);
+
+  useEffect(() => {
+    document.getElementById('1').textContent = `You weigh ${weight}, you ok with that?`;
+  }, [ weight ]);
+
   return (
     <div className="App">
+      <h1 id="1" class="comment">Comments here</h1>
+      <p>Current Weight: {weight}</p>
+      <p>Age: {age}</p>
+      <p>Month: {month}</p>
+      <button onClick={() => setWeight(weight + 15)}>
+        {todos[0].text}
+      </button>
+      <p></p>
+      <button onClick={() => setAge(age + 1)}>
+        {todos[1].text}
+      </button>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
